b7ec5aaf2990ac8d32ce288ccbd181938be31eb4
Jimyag <jimyag@126.com>
2022-05-08 22:32:40 +0800

    test: 更新index的GetAll()和IsInIndex()

--- core/model/index_test.go
+++ core/model/index_test.go
@@ -77,3 +77,162 @@ func TestIndex_DupAdd(t *testing.T) {
 	}
 	wg.Wait()
 }
+
+//
+//  TestIndex_GetAll
+//  @Description: Test getting all indexes
+//  @param t
+//
+func TestIndex_GetAll(t *testing.T) {
+	index := NewIndex()
+	name := "test"
+	for i := 0; i < 10; i++ {
+		err := index.Add(name, i)
+		require.NoError(t, err)
+	}
+	// 获取有索引的
+	docs, err := index.GetAll(name)
+	require.NoError(t, err)
+	require.Len(t, *docs, 10)
+
+	// 获取没有索引的
+	docs, err = index.GetAll(name + "1")
+	require.ErrorIs(t, err, ErrIndexNotFound)
+	require.Nil(t, docs)
+}
+
+//
+//  TestIndex_DupGetAll
+//  @Description: Test getting all indexes concurrently
+//  @param t
+//
+func TestIndex_DupGetAll(t *testing.T) {
+	index := NewIndex()
+	baseName := "test"
+	n := 100
+	wg := sync.WaitGroup{}
+	// 给 n/10 个分词添加索引
+	for i := 0; i < n/10; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			for j := 0; j < n; j++ {
+				err := index.Add(baseName+strconv.Itoa(i), j)
+				require.NoError(t, err)
+			}
+		}()
+	}
+	wg.Wait()
+
+	// 已存在的索引
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			data, err := index.GetAll(baseName + strconv.Itoa(i%10))
+			require.NoError(t, err)
+			require.Len(t, *data, n)
+		}()
+	}
+
+	// 不存在的索引
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			data, err := index.GetAll(baseName + strconv.Itoa(i+10000))
+			require.ErrorIs(t, err, ErrIndexNotFound)
+			require.Nil(t, data)
+		}()
+	}
+	wg.Wait()
+}
+
+//
+//  TestIndex_IsInIndex
+//  @Description: Test checking if a doc is in an index
+//	测试某个文档是否在索引中
+//  @param t
+//
+func TestIndex_IsInIndex(t *testing.T) {
+	index := NewIndex()
+
+	name := "test"
+	docId := 1
+
+	err := index.Add(name, docId)
+	require.NoError(t, err)
+
+	// 有分词 在索引中
+	find, err := index.IsInIndex(name, docId)
+	require.NoError(t, err)
+	require.True(t, find)
+
+	// 有分词，不在索引
+	find, err = index.IsInIndex(name, docId+1)
+	require.NoError(t, err)
+	require.True(t, !find)
+
+	// 没有分词
+	find, err = index.IsInIndex(name+"1", docId)
+	require.ErrorIs(t, err, ErrIndexNotFound)
+	require.True(t, !find)
+}
+
+func TestIndex_DuoIsInIndex(t *testing.T) {
+	index := NewIndex()
+	baseName := "test"
+	n := 100
+	wg := sync.WaitGroup{}
+	// 给 n/10 个分词添加索引
+	for i := 0; i < n/10; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			for j := 0; j < n; j++ {
+				err := index.Add(baseName+strconv.Itoa(i), j)
+				require.NoError(t, err)
+			}
+		}()
+	}
+	wg.Wait()
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			find, err := index.IsInIndex(baseName+strconv.Itoa(i/10), i)
+			require.NoError(t, err)
+			require.True(t, find)
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			find, err := index.IsInIndex(baseName+strconv.Itoa(i/10), i+n)
+			require.NoError(t, err)
+			require.True(t, !find)
+		}()
+	}
+
+	for i := 0; i < n; i++ {
+		wg.Add(1)
+		i := i
+		go func() {
+			defer wg.Done()
+			find, err := index.IsInIndex(baseName+strconv.Itoa(i/10+10), i)
+			require.ErrorIs(t, err, ErrIndexNotFound)
+			require.True(t, !find)
+		}()
+	}
+
+	wg.Wait()
+}
