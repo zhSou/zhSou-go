cf035d4524d51d09a6fa41a2a15cadd8e9ace41e
zzq <i@zhangzqs.cn>
2022-05-30 00:45:50 +0800

    feat: 实现倒排索引数据结构

---
+++ core/invertedindex/invertedindex.go
@@ -0,0 +1,48 @@
+package invertedindex
+
+import (
+	"encoding/gob"
+	"io"
+	"sort"
+)
+
+type invertedIndex struct {
+	Data map[string][]int
+}
+
+func NewInvertedIndex() *invertedIndex {
+	return &invertedIndex{
+		Data: make(map[string][]int),
+	}
+}
+
+func LoadInvertedIndexFromDisk(r io.Reader) (*invertedIndex, error) {
+	ii := invertedIndex{}
+	err := gob.NewDecoder(r).Decode(&ii)
+	if err != nil {
+		return nil, err
+	}
+	return &ii, nil
+}
+
+func (i *invertedIndex) SaveToDisk(w io.Writer) error {
+	err := gob.NewEncoder(w).Encode(*i)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (i *invertedIndex) Add(word string, id int) {
+	i.Data[word] = append(i.Data[word], id)
+}
+
+func (i *invertedIndex) Get(word string) []int {
+	return i.Data[word]
+}
+
+func (i *invertedIndex) Sort() {
+	for _, ids := range i.Data {
+		sort.Ints(ids)
+	}
+}
---
+++ core/invertedindex/invertedindex_test.go
@@ -0,0 +1,50 @@
+package invertedindex
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewInvertedIndex(t *testing.T) {
+	assert.NotNil(t, NewInvertedIndex())
+}
+
+func TestInvertedIndex_Add(t *testing.T) {
+	ii := NewInvertedIndex()
+	ii.Add("w1", 5)
+	ii.Add("w1", 8)
+	ii.Add("w1", 1)
+	ii.Add("w1", 6)
+	assert.Equal(t, []int{5, 8, 1, 6}, ii.Get("w1"))
+}
+
+func TestInvertedIndex_Sort(t *testing.T) {
+	ii := NewInvertedIndex()
+	ii.Add("w1", 5)
+	ii.Add("w1", 8)
+	ii.Add("w1", 1)
+	ii.Add("w1", 6)
+	ii.Sort()
+	assert.Equal(t, []int{1, 5, 6, 8}, ii.Get("w1"))
+}
+
+func TestInvertedIndex_SaveToDisk(t *testing.T) {
+	ii := NewInvertedIndex()
+	ii.Add("w1", 5)
+	ii.Add("w1", 8)
+	ii.Add("w1", 1)
+	ii.Add("w1", 6)
+	ii.Sort()
+
+	buf := bytes.NewBuffer([]byte{})
+	err := ii.SaveToDisk(buf)
+	if err != nil {
+		panic(err)
+	}
+	ifd, err := LoadInvertedIndexFromDisk(buf)
+	if err != nil {
+		return
+	}
+	assert.Equal(t, ii.Data, ifd.Data)
+}
