aa0c6ba7c79ca8b5239eaa5a996699180de422ad
zzq <i@zhangzqs.cn>
2022-05-29 18:48:38 +0800

    update(test): 完善二分查找测试

--- util/algorithm/binary/binary_test.go
+++ util/algorithm/binary/binary_test.go
@@ -8,12 +8,19 @@ import (
 func TestFindFirstBigger(t *testing.T) {
 	s1 := []int{1, 6, 21, 90, 100}
 	// 正常情况
-	assert.Equal(t, FindFirstBigger(&SliceAccessible{s1}, 1), 1)
-	assert.Equal(t, FindFirstBigger(&SliceAccessible{s1}, 21), 3)
-	// 比4大的数找不到
-	assert.Equal(t, FindFirstBigger(&SliceAccessible{s1}, 100), -1)
+	assert.Equal(t, 0, FindFirstBigger(&SliceAccessible{s1}, -1))
+	assert.Equal(t, 0, FindFirstBigger(&SliceAccessible{s1}, 0))
+
+	assert.Equal(t, 1, FindFirstBigger(&SliceAccessible{s1}, 1))
+	assert.Equal(t, 1, FindFirstBigger(&SliceAccessible{s1}, 4))
+	assert.Equal(t, 3, FindFirstBigger(&SliceAccessible{s1}, 21))
+
+	assert.Equal(t, 4, FindFirstBigger(&SliceAccessible{s1}, 91))
+	// 比100,104大的数找不到，返回-1
+	assert.Equal(t, -1, FindFirstBigger(&SliceAccessible{s1}, 100))
+	assert.Equal(t, -1, FindFirstBigger(&SliceAccessible{s1}, 104))
 
 	// 不存在任何数字，故查找到的索引值恒为-1
-	assert.Equal(t, FindFirstBigger(&SliceAccessible{[]int{}}, 1), -1)
-	assert.Equal(t, FindFirstBigger(&SliceAccessible{[]int{}}, 4), -1)
+	assert.Equal(t, -1, FindFirstBigger(&SliceAccessible{[]int{}}, 1))
+	assert.Equal(t, -1, FindFirstBigger(&SliceAccessible{[]int{}}, 4))
 }
