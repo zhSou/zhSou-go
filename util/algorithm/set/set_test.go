76b070cb89031e97bfe1fabfae3e76c3c2e566f1
zzq <i@zhangzqs.cn>
2022-05-30 00:13:46 +0800

    update(set): 完善有序表的交并补运算测试

--- util/algorithm/set/set_test.go
+++ util/algorithm/set/set_test.go
@@ -5,9 +5,24 @@ import (
 	"testing"
 )
 
+func TestCross(t *testing.T) {
+	assert.Equal(t, []int{}, Cross([]int{}, []int{}))
+	assert.Equal(t, []int{}, Cross([]int{1}, []int{}))
+	assert.Equal(t, []int{}, Cross([]int{1, 2}, []int{3, 4}))
+	assert.Equal(t, []int{2, 3}, Cross([]int{1, 2, 3, 4}, []int{2, 3, 8, 7}))
+}
+
 func TestSum(t *testing.T) {
 	assert.Equal(t, []int{}, Sum([]int{}, []int{}))
 	assert.Equal(t, []int{1}, Sum([]int{1}, []int{}))
 	assert.Equal(t, []int{1, 2, 3, 4}, Sum([]int{1, 2}, []int{3, 4}))
 	assert.Equal(t, []int{1, 2, 3}, Sum([]int{1, 2}, []int{2, 3}))
+}
+
+func TestExclude(t *testing.T) {
+	assert.Equal(t, []int{}, Exclude([]int{1, 2}, []int{1, 2}))
+	assert.Equal(t, []int{}, Exclude([]int{}, []int{}))
+	assert.Equal(t, []int{1, 2, 3, 4}, Exclude([]int{1, 2, 3, 4}, []int{}))
+	assert.Equal(t, []int{1, 2}, Exclude([]int{1, 2, 3, 4}, []int{3, 4}))
+	assert.Equal(t, []int{1, 4}, Exclude([]int{1, 2, 3, 4}, []int{2, 3}))
 }
