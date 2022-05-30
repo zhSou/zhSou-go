cac11475173e10c46e8552086d5c3ea6d7a6246c
zzq <i@zhangzqs.cn>
2022-05-30 10:51:31 +0800

    feat: 实现倒排索引的查询!!!

--- global/global.go
+++ global/global.go
@@ -1,6 +1,8 @@
 package global
 
 import (
+	"github.com/zhSou/zhSou-go/core/config"
+	"github.com/zhSou/zhSou-go/core/dataset"
 	"github.com/zhSou/zhSou-go/core/stopword"
 	"github.com/zhSou/zhSou-go/util/tokenizer"
 )
@@ -8,9 +10,20 @@ import (
 var (
 	Tokenizer     *tokenizer.Tokenizer
 	StopWordTable *stopword.Table
+	DataReader    *dataset.DataReader
 )
 
-func InitGlobal() {
+func InitGlobal(conf *config.Config) {
 	Tokenizer = tokenizer.NewTokenizer()
 	StopWordTable = stopword.NewTable()
+	StopWordTable.LoadStopWord(conf.StopWordPath)
+	// 绑定一些额外的停用词规则
+	StopWordTable.BindExtraRule(func(word string) bool {
+		// 小于等于一字符的为停用词
+		if len(word) <= 1 {
+			return true
+		}
+		return false
+	})
+	DataReader, _ = dataset.NewDataReader(conf.DataIndexPaths, conf.DataPaths)
 }
--- main.go
+++ main.go
@@ -11,77 +11,85 @@ import (
 	"os"
 )
 
-var conf = config.Config{
-	DataPaths:             []string{},
-	DataIndexPaths:        []string{},
-	CsvPaths:              []string{},
-	InvertedIndexFilePath: "D:\\inverted_index.inv",
-	StopWordPath:          "D:\\stop_words.txt",
-}
-
-func InitConfig(n int) {
+func InitConfig(n int) *config.Config {
+	var conf = config.Config{
+		DataPaths:             []string{},
+		DataIndexPaths:        []string{},
+		CsvPaths:              []string{},
+		InvertedIndexFilePath: "D:\\inverted_index.inv",
+		StopWordPath:          "D:\\stop_words.txt",
+	}
 	for i := 0; i < n; i++ {
 		conf.DataPaths = append(conf.DataPaths, fmt.Sprintf("D:\\data\\wukong_100m_%d.dat", i))
 		conf.DataIndexPaths = append(conf.DataIndexPaths, fmt.Sprintf("D:\\index\\wukong_100m_%d.idx", i))
 		conf.CsvPaths = append(conf.CsvPaths, fmt.Sprintf("D:\\input\\wukong_100m_%d.csv", i))
 	}
+	return &conf
+}
+
+var conf *config.Config
+var n = 1
+
+func ImportCsvHandler() {
+	// TODO 可以并行优化
+	for i := 0; i < n; i++ {
+		dataset.ConvCsvMakeIndexFile(conf.CsvPaths[i], conf.DataPaths[i], conf.DataIndexPaths[i])
+	}
+}
+
+func MakeInvertedIndexHandler() {
+	dataReader := global.DataReader
+	log.Println("总数据记录数：", dataReader.Len())
+
+	inv := invertedindex.NewInvertedIndex()
+	// TODO 可以并行优化
+	for i := uint32(0); i < dataReader.Len(); i++ {
+		dataRecord, err := dataReader.Read(i)
+		if err != nil {
+			return
+		}
+		for _, word := range global.Tokenizer.Cut(dataRecord.Text) {
+			if global.StopWordTable.IsStopWord(word) {
+				// 停用词过滤
+				continue
+			}
+			inv.Add(word, int(i))
+		}
+		if i%10000 == 0 {
+			log.Printf("当前建立倒排索引百分比 %.2f", float64(i)/float64(dataReader.Len()))
+		}
+	}
+	// 倒排索引升序排序
+	inv.Sort()
+	invFile, _ := os.OpenFile(conf.InvertedIndexFilePath, os.O_RDWR|os.O_CREATE, 0777)
+	defer invFile.Close()
+	_ = inv.SaveToDisk(invFile)
+	_ = invFile.Sync()
+	log.Println("倒排索引文件已存盘：", conf.InvertedIndexFilePath)
+}
+
+func QueryInvertedIndexHandler() {
+	file, _ := os.Open(conf.InvertedIndexFilePath)
+	defer file.Close()
+	inv, _ := invertedindex.LoadInvertedIndexFromDisk(file)
+	is := inv.Get("手机")
+	for i, id := range is {
+		if i > 15 {
+			break
+		}
+		record, _ := global.DataReader.Read(uint32(id))
+		fmt.Println(record)
+	}
 }
 
 func main() {
-	n := 1
-	InitConfig(n)
-	global.InitGlobal()
-	global.StopWordTable.LoadStopWord(conf.StopWordPath)
-	// 绑定一些额外的停用词规则
-	global.StopWordTable.BindExtraRule(func(word string) bool {
-		// 小于等于一字符的为停用词
-		if len(word) <= 1 {
-			return true
-		}
-		return false
-	})
+	conf = InitConfig(n)
+	global.InitGlobal(conf)
+
 	mainMenu := menu.NewMenu("主菜单")
-	mainMenu.AddItem("csv数据集导入", func() {
-		// TODO 可以优化成并发执行
-		for i := 0; i < n; i++ {
-			dataset.ConvCsvMakeIndexFile(conf.CsvPaths[i], conf.DataPaths[i], conf.DataIndexPaths[i])
-		}
-	})
-	mainMenu.AddItem("构建倒排索引", func() {
-		dataReader, _ := dataset.NewDataReader(conf.DataIndexPaths, conf.DataPaths)
-		log.Println("总数据记录数：", dataReader.Len())
-
-		inv := invertedindex.NewInvertedIndex()
-		for i := uint32(0); i < dataReader.Len(); i++ {
-			dataRecord, err := dataReader.Read(i)
-			if err != nil {
-				return
-			}
-			for _, word := range global.Tokenizer.Cut(dataRecord.Text) {
-				if global.StopWordTable.IsStopWord(word) {
-					// 停用词过滤
-					continue
-				}
-				inv.Add(word, int(i))
-			}
-			if i%10000 == 0 {
-				log.Printf("当前建立倒排索引百分比 %.2f", float64(i)/float64(dataReader.Len()))
-			}
-		}
-		// 倒排索引升序排序
-		inv.Sort()
-		invFile, _ := os.OpenFile(conf.InvertedIndexFilePath, os.O_RDWR|os.O_CREATE, 0777)
-		defer invFile.Close()
-		_ = inv.SaveToDisk(invFile)
-		_ = invFile.Sync()
-		log.Println("倒排索引文件已存盘：", conf.InvertedIndexFilePath)
-	})
-
-	mainMenu.AddItem("查询倒排索引", func() {
-		file, _ := os.Open(conf.InvertedIndexFilePath)
-		defer file.Close()
-
-	})
+	mainMenu.AddItem("csv数据集导入", ImportCsvHandler)
+	mainMenu.AddItem("构建倒排索引", MakeInvertedIndexHandler)
+	mainMenu.AddItem("查询倒排索引", QueryInvertedIndexHandler)
 	mainMenu.AddExitItem("退出")
 	mainMenu.Loop()
 }
