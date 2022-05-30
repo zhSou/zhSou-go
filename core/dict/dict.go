9181a8d08a5d7a9da4680deebc4a50c008a6b475
zzq <i@zhangzqs.cn>
2022-05-30 14:58:58 +0800

    feat: 抽象出字典功能

---
+++ core/dict/dict.go
@@ -0,0 +1,51 @@
+package dict
+
+import (
+	"encoding/gob"
+	"io"
+)
+
+// Dict 分词到分词id的映射
+type Dict struct {
+	Data map[string]int
+}
+
+func NewDict() *Dict {
+	return &Dict{
+		Data: make(map[string]int),
+	}
+}
+
+func Load(r io.Reader) (*Dict, error) {
+	var dict Dict
+	err := gob.NewDecoder(r).Decode(&dict)
+	if err != nil {
+		return nil, err
+	}
+	return &dict, nil
+}
+
+func (d *Dict) Save(w io.Writer) error {
+	err := gob.NewEncoder(w).Encode(*d)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (d *Dict) Put(word string) int {
+	if id := d.Get(word); id != -1 {
+		return id
+	}
+	wordId := len(d.Data)
+	d.Data[word] = wordId
+	return wordId
+}
+
+func (d *Dict) Get(word string) int {
+	i, ok := d.Data[word]
+	if !ok {
+		return -1
+	}
+	return i
+}
---
+++ core/dict/dict_test.go
@@ -0,0 +1,46 @@
+package dict
+
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestNewDict(t *testing.T) {
+	assert.NotNil(t, NewDict())
+}
+
+func TestDict_Put(t *testing.T) {
+	dict := NewDict()
+	assert.Equal(t, 0, dict.Put("A"))
+	assert.Equal(t, 0, dict.Put("A"))
+	assert.Equal(t, 1, dict.Put("B"))
+	assert.Equal(t, 1, dict.Put("B"))
+	assert.Equal(t, 2, dict.Put("C"))
+	assert.Equal(t, 0, dict.Put("A"))
+}
+
+func TestDict_Get(t *testing.T) {
+	dict := NewDict()
+	// 正常情况
+	assert.Equal(t, 0, dict.Put("A"))
+	assert.Equal(t, 0, dict.Get("A"))
+	assert.Equal(t, 1, dict.Put("B"))
+	assert.Equal(t, 1, dict.Get("B"))
+	assert.Equal(t, 0, dict.Get("A"))
+
+	// 无效情况
+	assert.Equal(t, -1, dict.Get("C"))
+}
+
+func TestDict_Save(t *testing.T) {
+	dict := NewDict()
+	dict.Put("A")
+	dict.Put("B")
+	buf := bytes.NewBuffer([]byte{})
+	_ = dict.Save(buf)
+
+	loadDict, _ := Load(buf)
+	assert.Equal(t, 0, loadDict.Get("A"))
+	assert.Equal(t, 1, loadDict.Get("B"))
+}
