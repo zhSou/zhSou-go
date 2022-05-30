4254798854478e1337c6dab7a9ef83e26dc3db52
zzq <i@zhangzqs.cn>
2022-05-30 13:39:44 +0800

    update: 倒排索引模块支持添加带去重的多分词到id的映射

--- core/invertedindex/invertedindex.go
+++ core/invertedindex/invertedindex.go
@@ -2,6 +2,7 @@ package invertedindex
 
 import (
 	"encoding/gob"
+	"github.com/zhSou/zhSou-go/util/algorithm/set"
 	"io"
 	"sort"
 )
@@ -35,6 +36,12 @@ func (i *invertedIndex) SaveToDisk(w io.Writer) error {
 
 func (i *invertedIndex) Add(word string, id int) {
 	i.Data[word] = append(i.Data[word], id)
+}
+
+func (i *invertedIndex) AddWords(words []string, id int) {
+	for _, word := range set.Deduplication[string](words) {
+		i.Data[word] = append(i.Data[word], id)
+	}
 }
 
 func (i *invertedIndex) Get(word string) []int {
