71d103278c9fa9dbe32241eeb29709c63a4eb3fe
zzq <i@zhangzqs.cn>
2022-05-30 10:34:29 +0800

    feat: filesystem新增新建文件夹判定

--- util/filesystem/filesystem.go
+++ util/filesystem/filesystem.go
@@ -1,18 +1,39 @@
 package filesystem
 
 import (
+	"log"
 	"os"
 	"strings"
 )
+
+func Exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true, nil
+	}
+	if os.IsNotExist(err) {
+		return false, nil
+	}
+	return false, err
+}
 
 func MakeDir(filepath string) error {
 	i := strings.LastIndexFunc(filepath, func(r rune) bool {
 		return r == '\\'
 	})
 	dirPath := filepath[:i]
-	err := os.MkdirAll(dirPath, 0777)
+	exist, err := Exists(dirPath)
 	if err != nil {
 		return err
 	}
+	if exist {
+		log.Println("路径已存在：", dirPath)
+		return nil
+	}
+	err = os.MkdirAll(dirPath, 0777)
+	if err != nil {
+		return err
+	}
+	log.Println("新建文件夹：", dirPath)
 	return nil
 }
