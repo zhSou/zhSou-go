7771696d8a5b1546b3aa129f660801e6503f2460
Jimyag <jimyag@126.com>
2022-06-01 22:52:15 +0800

    test: 移除测试的初始化

--- service/search_test.go
+++ service/search_test.go
@@ -1,64 +1,17 @@
 package service
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
 
-	"github.com/zhSou/zhSou-go/core/config"
+	config2 "github.com/zhSou/zhSou-go/config"
 	"github.com/zhSou/zhSou-go/global"
 )
 
 func init() {
 	rand.Seed(time.Now().Unix())
 
-}
-
-//const pathPrefix = "C:\\Users\\zzq\\Desktop\\新建文件夹 (2)"
-const pathPrefix = "D:"
-
-// InitConfigLight 测试时候可以用这个轻量级配置文件，只加载一个数据集
-func InitConfigLight() *config.Config {
-	var conf = config.Config{
-		DataPaths:                   []string{},
-		DataIndexPaths:              []string{},
-		CsvPaths:                    []string{},
-		InvertedIndexFilePath:       pathPrefix + "\\light\\inverted_index.inv",
-		DictPath:                    pathPrefix + "\\light\\dict.dic",
-		StopWordPath:                pathPrefix + "\\stop_words.txt",
-		ImportCsvCoroutines:         4,
-		MakeInvertedIndexCoroutines: 8,
-		PathLength:                  1,
-		SearchLruMaxCapacity:        20,
-	}
-	for i := 0; i < conf.PathLength; i++ {
-		conf.DataPaths = append(conf.DataPaths, fmt.Sprintf("%s\\light\\data\\wukong_100m_%d.dat", pathPrefix, i))
-		conf.DataIndexPaths = append(conf.DataIndexPaths, fmt.Sprintf("%s\\light\\index\\wukong_100m_%d.idx", pathPrefix, i))
-		conf.CsvPaths = append(conf.CsvPaths, fmt.Sprintf("%s\\input\\wukong_100m_%d.csv", pathPrefix, i))
-	}
-	return &conf
-}
-
-func InitConfig() *config.Config {
-	var conf = config.Config{
-		DataPaths:                   []string{},
-		DataIndexPaths:              []string{},
-		CsvPaths:                    []string{},
-		InvertedIndexFilePath:       pathPrefix + "\\inverted_index.inv",
-		DictPath:                    pathPrefix + "\\dict.dic",
-		StopWordPath:                pathPrefix + "\\stop_words.txt",
-		ImportCsvCoroutines:         4,
-		MakeInvertedIndexCoroutines: 8,
-		PathLength:                  256,
-		SearchLruMaxCapacity:        20,
-	}
-	for i := 0; i < conf.PathLength; i++ {
-		conf.DataPaths = append(conf.DataPaths, fmt.Sprintf("%s\\data\\wukong_100m_%d.dat", pathPrefix, i))
-		conf.DataIndexPaths = append(conf.DataIndexPaths, fmt.Sprintf("%s\\index\\wukong_100m_%d.idx", pathPrefix, i))
-		conf.CsvPaths = append(conf.CsvPaths, fmt.Sprintf("%s\\input\\wukong_100m_%d.csv", pathPrefix, i))
-	}
-	return &conf
 }
 
 func generatorSearchQuery() []string {
@@ -98,7 +51,7 @@ func generatorSearchQuery() []string {
 //PASS
 //ok      github.com/zhSou/zhSou-go/service       66.660s
 func BenchmarkSearchLight(b *testing.B) {
-	conf := InitConfigLight()
+	conf := config2.InitConfigLight()
 	global.InitGlobal(conf)
 	queries := generatorSearchQuery()
 	n := len(queries)
@@ -116,7 +69,7 @@ func BenchmarkSearchLight(b *testing.B) {
 //PASS
 //ok      github.com/zhSou/zhSou-go/service       112.430s
 func BenchmarkSearchAll(b *testing.B) {
-	conf := InitConfig()
+	conf := config2.InitConfig()
 	global.InitGlobal(conf)
 	queries := generatorSearchQuery()
 	n := len(queries)
